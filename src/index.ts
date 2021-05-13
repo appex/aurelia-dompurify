2c4d9e8f1742ce4e5469b68046b8de7ead932d87
David A. Sjøen <david.sjoen@appex.no>
2021-05-13 19:04:37 +0200

    Edited README

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import { HTMLSanitizer } from "aurelia-templating-resources";
 import { DOMPurifySanitizer } from "./dom-purify-sanitizer";
 
 export function configure(config: FrameworkConfiguration): void {
-  console.log('Configuring DOMPurifySanitizer');
   config.singleton(HTMLSanitizer, DOMPurifySanitizer);
 }
