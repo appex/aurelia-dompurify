cebd1099df9154488d8c555b15f653f032a6186c
David A. Sjøen <david.sjoen@appex.no>
2021-05-16 01:06:52 +0200

    Made it public

--- dist/native-modules/index.js
+++ dist/native-modules/index.js
@@ -1,7 +1,6 @@
 import { HTMLSanitizer } from "aurelia-templating-resources";
 import { DOMPurifySanitizer } from "./dom-purify-sanitizer";
 export function configure(config) {
-    console.log('Configuring DOMPurifySanitizer');
     config.singleton(HTMLSanitizer, DOMPurifySanitizer);
 }
 
